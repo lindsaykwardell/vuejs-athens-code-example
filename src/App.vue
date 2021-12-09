da4a98edf46f5ba6f547072935268f1ee44ea090
Lindsay Wardell <three060@gmail.com>
2021-12-08 18:45:55 -0800

    <script setup> - App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+<script setup lang="ts">
+import { ref, computed } from "vue";
 import { useRoute } from "vue-router";
 import type { Todo } from "./types/Todo";
 
@@ -7,73 +7,53 @@ import Header from "./components/Header.vue";
 import TodoCard from "./components/TodoCard.vue";
 import Footer from "./components/Footer.vue";
 
-export default defineComponent({
-  setup() {
-    const todos = ref<Todo[]>([]);
-    const route = useRoute();
+const todos = ref<Todo[]>([]);
+const route = useRoute();
 
-    const visibleTodos = computed(() => {
-      switch (route.name) {
-        case "home":
-          return todos.value;
-        case "active":
-          return todos.value.filter((todo) => !todo.completed);
-        case "completed":
-          return todos.value.filter((todo) => todo.completed);
-      }
-    });
-
-    const remainingCount = computed(
-      () => todos.value.filter((todo) => !todo.completed).length
-    );
-    const totalCount = computed(() => todos.value.length);
-
-    function addTodo(todo: string) {
-      todos.value.push({
-        id: todos.value.length,
-        title: todo,
-        completed: false,
-      });
-    }
-
-    function updateTodo(updatedTodo: Todo) {
-      todos.value = todos.value.map((todo) =>
-        todo.id === updatedTodo.id ? { ...todo, ...updatedTodo } : todo
-      );
-    }
-
-    function deleteTodo(id: number) {
-      todos.value = todos.value.filter((todo) => todo.id !== id);
-    }
-
-    function toggleAll() {
-      todos.value = todos.value.map((todo) => ({
-        ...todo,
-        completed: remainingCount.value !== 0,
-      }));
-    }
-
-    function clearCompleted() {
-      todos.value = todos.value.filter((todo) => !todo.completed);
-    }
-
-    return {
-      visibleTodos,
-      remainingCount,
-      totalCount,
-      addTodo,
-      updateTodo,
-      deleteTodo,
-      toggleAll,
-      clearCompleted,
-    };
-  },
-  components: {
-    Header,
-    TodoCard,
-    Footer,
-  },
+const visibleTodos = computed(() => {
+  switch (route.name) {
+    case "home":
+      return todos.value;
+    case "active":
+      return todos.value.filter((todo) => !todo.completed);
+    case "completed":
+      return todos.value.filter((todo) => todo.completed);
+  }
 });
+
+const remainingCount = computed(
+  () => todos.value.filter((todo) => !todo.completed).length
+);
+const totalCount = computed(() => todos.value.length);
+
+function addTodo(todo: string) {
+  todos.value.push({
+    id: todos.value.length,
+    title: todo,
+    completed: false,
+  });
+}
+
+function updateTodo(updatedTodo: Todo) {
+  todos.value = todos.value.map((todo) =>
+    todo.id === updatedTodo.id ? { ...todo, ...updatedTodo } : todo
+  );
+}
+
+function deleteTodo(id: number) {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+}
+
+function toggleAll() {
+  todos.value = todos.value.map((todo) => ({
+    ...todo,
+    completed: remainingCount.value !== 0,
+  }));
+}
+
+function clearCompleted() {
+  todos.value = todos.value.filter((todo) => !todo.completed);
+}
 </script>
 
 <template>
